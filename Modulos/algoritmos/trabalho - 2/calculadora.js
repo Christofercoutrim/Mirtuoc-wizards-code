4e30f08a3471685d8e64c136a04b66860e17f2b9
Christofercoutrim <christofercoutrin@gmail.com>
2021-02-03 00:19:25 -0300

    calculadora

---
+++ Modulos/algoritmos/trabalho - 2/calculadora.js
@@ -0,0 +1,15 @@
+const a = 12
+const b = 10
+
+const soma = `A soma dos valores citados é igual a ${a+b}`
+
+const subtracao = `A subtração dos valores citados é igual a ${a-b}`
+
+const multiplicacao = `A multiplicação dos valores citados é igual a ${a*b}`
+
+const divisao = `A divisão dos valores citados é igual a ${a/b}`
+
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
