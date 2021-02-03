b37efdb815460032e9fe7c8896ddba36045d3d23
Christofercoutrim <christofercoutrin@gmail.com>
2021-02-02 23:58:19 -0300

    aula 1

---
+++ Modulos/algoritmos/trabalho - 1/vanguard.js
@@ -0,0 +1,8 @@
+const trotsky =  'Os homens não têm muito respeito pelos outros porque têm pouco até por si próprios.';
+
+const bethoven = 'Tenho paciência e penso: todo o mal traz consigo algum bem.';
+
+console.log('TROTSKY');
+console.log(trotsky);
+console.log('BETHOVEN');
+console.log(bethoven);
