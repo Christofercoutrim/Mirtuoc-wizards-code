77fc16f49573fcb95800fdaaec368995bda3c220
Christofercoutrim <christofercoutrin@gmail.com>
2021-02-03 00:00:34 -0300

    aula 2

---
+++ Modulos/algoritmos/Aula-2/aula.js
@@ -0,0 +1,49 @@
+const frango = 'peito assado'
+
+let assado = 'carne'
+assado = 'porco'
+
+// Nunca usar
+var cachorro = 'só chines come'
+
+// ----------------------------------------------------------------------
+//o que posso colocar 
+
+//string
+const string = 'uma frase, uma palavra, um dialogo'
+
+//int - inteiro
+const int = 666
+
+//double
+const double = 75.66
+
+//boolean
+const boolean = false
+ 
+//-----------------------------------------------
+
+//console.log('frase aleatoria que ninguem se importa')
+
+//--------------------------------------------------------------------------
+// calculo
+
+const a = 75
+const b = 556
+
+// soma = +
+// subtração = -
+// multiplicação = *
+// divisao = /
+
+//console.log(a*b)
+
+//---------------------------------------------------------------------
+// formatação
+
+const nome = 'Seu Zé da esquina'
+const herança = 'R$: 2.000.000.000'
+const parentesco = 'sobrinha'
+
+console.log(`Olá ${nome}!
+Eu irei te matar pois você tem ${herança}, e eu irei ganhar isso pois sou sua ${parentesco}`)
