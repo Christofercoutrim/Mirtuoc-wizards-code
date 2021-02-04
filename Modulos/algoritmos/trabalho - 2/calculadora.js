21f3d8431a33749de58b12e8ffaec4226f41002f
Christofercoutrim <christofercoutrin@gmail.com>
2021-02-04 20:41:40 -0300

    final

--- Modulos/algoritmos/trabalho - 2/calculadora.js
+++ Modulos/algoritmos/trabalho - 2/calculadora.js
@@ -1,5 +1,10 @@
-const a = 12
-const b = 10
+let a = 12
+let b = 10
+let c = 8
+let valorPotencia = 3
+let valorRaiz = 2
+
+//usei let para que caso eu precise deixar a equação de uma maneira na qual o cliente possa escolher os valores 
 
 const soma = `A soma dos valores citados é igual a ${a+b}`
 
@@ -9,9 +14,15 @@ const multiplicacao = `A multiplicação dos valores citados é igual a ${a*b}`
 
 const divisao = `A divisão dos valores citados é igual a ${a/b}`
 
+const potencia = `a potencia do valor citado é igual a ${Math.pow(c, valorPotencia)}`
+
+const raiz = `a raiz quadrada do valor citado é igual a ${Math.sqrt(valorRaiz)}`
+
 console.log(soma)
 console.log(subtracao)
 console.log(multiplicacao)
 console.log(divisao)
+console.log(potencia)
+console.log(raiz)
 
-// Achei mais descritivo citar separadamente cada equação, valeu demais 
+// Achei mais descritivo citar separadamente cada equação.
