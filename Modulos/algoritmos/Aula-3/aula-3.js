957afe5b1319cae4aef88a93792030c31d25598b
Christofercoutrim <christofercoutrin@gmail.com>
2021-02-05 00:02:47 -0300

    não preste atenção nessas anotações merdas

---
+++ Modulos/algoritmos/Aula-3/aula-3.js
@@ -0,0 +1,41 @@
+// if e else
+// if = se (isto acontecer){ isso aqui acontece}
+// else = em vez (isto acontecer) {}
+
+// = receber valor
+// == NUNCA USAR
+// === definir se é completamente igual
+
+// > maior que
+// < menor que
+// >= maior ou igual
+// <= menor ou igual
+
+// ! não existe (quando não tem valor)
+
+null // não existe
+undefined // não é definido então não existe
+
+// && e
+// || ou
+
+//-------------------------------------------------------------------------
+//funções
+// ++ acrecentar um valor
+
+//=-=-=-
+
+let valorpreedefinido = valor 
+
+acontecimento aleatorio(0, 1)
+
+function acontecimento aleatorio(recebeuAtaqueFatal, naoRecebeuAtaqueFatal){
+   if(recebeuAtaqueFatal = 0){return `Você morreu`}
+}
+
+//--------------------------------------------------------------------
+//se o Itachi for lutar ele ganha de qualquer um
+//batalhar uma comparação primeiro ataque de cada personagem
+//batalha definitiva ambos personagens até a morte de algum chegue a zero de vida
+//lista de personagens citadas pelo Lucca
+
