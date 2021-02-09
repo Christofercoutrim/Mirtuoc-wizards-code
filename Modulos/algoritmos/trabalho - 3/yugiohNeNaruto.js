a7180ea7f5985dc2b7e74c0d3fd2d1d1ca0a4e25
Christofercoutrim <christofercoutrin@gmail.com>
2021-02-08 22:48:42 -0300

    ola

--- Modulos/algoritmos/trabalho - 3/yugiohNeNaruto.js
+++ Modulos/algoritmos/trabalho - 3/yugiohNeNaruto.js
@@ -6,14 +6,17 @@ const Gai={name:'GAI',vida:720,damage:200}
 const itachi={name:'ITACHI',vida:700,damage:150}
 
 function battle(ninja1,ninja2){
-    ninja1.vida-ninja2.ataque
-    ninja2.vida-ninja1.ataque
+
     if(itachi === ninja1 || itachi === ninja2){
-        console.log(`${itachi.name} VENCEU`)
+        return `${itachi.name} VENCEU`
     }
-    else if(ninja1.vida>ninja2.vida){
-        console.log(`${ninja1.name} VENCEU`)}
-else(console.log(`${ninja2.name} VENCEU`))
+    else if(ninja1.vida-ninja2.ataque>ninja2.vida-ninja1.ataque){
+        return `${ninja1.name} VENCEU`
     }
+    else{ return`${ninja2.name} VENCEU`}
+    if(ninja1.vida===ninja2.vida)
+        return `A BATALHA EMPATOU`
+    }
+
     battle(naruto,sasuke)
     battle(itachi,kakashi)
