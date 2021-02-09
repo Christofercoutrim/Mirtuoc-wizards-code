8ed0911b0570f9915476eff6e6387f75121978a0
Christofercoutrim <christofercoutrin@gmail.com>
2021-02-08 22:03:08 -0300

    imc

--- Modulos/algoritmos/Aula-4/imc.js
+++ Modulos/algoritmos/Aula-4/imc.js
@@ -2,8 +2,8 @@ const dadosDaPessoa = {nome:'Bernadete',peso:57,altura:1.79}
 
 const imc = dadosDaPessoa.peso/(dadosDaPessoa.altura*dadosDaPessoa.altura)
 
-if (imc<29.9){
+if (imc<=29.9){
     console.log(`${dadosDaPessoa.nome}, você não está acima do peso`)
-}if(imc>30){
+}if(imc>=30){
     console.log(`${dadosDaPessoa.nome}, você está acima do peso`)
 }
